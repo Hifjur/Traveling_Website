bc9485732445cfe7b335c340d5ca14ead4b74173
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-11-25 14:15:11 +0600

    patch

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCMgdlZBafvbb8MoZDM70a46IwvHvXm7fQ",
-    authDomain: "tourist-webapp.firebaseapp.com",
-    projectId: "tourist-webapp",
-    storageBucket: "tourist-webapp.appspot.com",
-    messagingSenderId: "849071893788",
-    appId: "1:849071893788:web:341bba484380f155d99ed9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
   };
 
 export default firebaseConfig;
